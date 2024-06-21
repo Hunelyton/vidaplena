4a4dbd40c2c3778c44e57f152ac80bcf59d1f319
Hunelyton Mendes <hunel@tom>
2024-06-20 22:49:27 -0300

    commit inicial

---
+++ escoladeministerios/src/main/java/br/com/vidaplena/escoladeministerios/alunos/Alunos.java
@@ -0,0 +1,65 @@
+package br.com.vidaplena.escoladeministerios.alunos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cadastroaluno")
+public class Alunos {
+	
+	
+	@Id
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private Long id;
+
+	@Column(length = 100, nullable = false)
+	private String nome;
+	
+	@Column(length = 20, nullable = false)
+	private String telefone;
+
+	@Column(length = 100, nullable = false)
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
