ca00e71b9aa4643fd824fb1935a461ce37dcdc86
Hunelyton Mendes <hunel@tom>
2024-06-22 12:49:37 -0300

    ajustes no insert

--- escoladeministerios/src/main/java/br/com/vidaplena/escoladeministerios/teste/Teste.java
+++ escoladeministerios/src/main/java/br/com/vidaplena/escoladeministerios/teste/Teste.java
@@ -8,36 +8,35 @@ import br.com.vidaplena.escoladeministerios.alunos.model.Alunos;
 
 public class Teste {
 
-	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("escoladeministeriosPU");
+	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(null);
 	private static EntityManager entityManager = entityManagerFactory.createEntityManager();
 	
 	public static void main(String[] args) {
 		
 		//Find
-		Alunos alunos = entityManager.find(Alunos.class, 1);
-		System.out.println("Nome do Aluno: " + alunos.getNome());
+	//	Alunos alunos = entityManager.find(Alunos.class, 1);
+	//	System.out.println("Nome do Aluno: " + alunos.getNome());
 	
 		// INSERT
-		Alunos cliente = new Alunos();
-		alunos.setNome("Maria Teste");
-//		
-//		entityManager.getTransaction().begin();
-//		entityManager.persist(cliente);
-//		entityManager.getTransaction().commit();
+		Alunos alunos = new Alunos();
+		alunos.setNome("Maria Teste");		
+		entityManager.getTransaction().begin();
+		entityManager.persist(alunos);
+		entityManager.getTransaction().commit();
 		
 		// DELETE
 //		Cliente cliente = entityManager.find(Cliente.class, 2);
 //		entityManager.getTransaction().begin();
-//		entityManager.remove(cliente);
+//		entityManager.remove(alunos);
 //		entityManager.getTransaction().commit();
 		
 		// MODIFY
-//		Cliente cliente = new Cliente();
-//		cliente.setId(1);
-//		cliente.setNome("FACEBOOK INC.");
+//		Alunos alunos = new Alunos();
+//		alunos.setId(1);
+//		alunos.setNome("José dos Santos");
 		
 //		entityManager.getTransaction().begin();
-//		entityManager.merge(cliente);
+//		entityManager.merge(alunos);
 //		entityManager.getTransaction().commit();
 
 		entityManager.close();
